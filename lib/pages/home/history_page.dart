e602ac0cf768016436cac0e7f99d4c64d8f379e8
madamiec19 <madamiec19@gmail.com>
2024-01-08 19:57:27 +0100

    baza szefa in progress

--- lib/pages/home/history_page.dart
+++ lib/pages/home/history_page.dart
@@ -7,6 +7,7 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 enum View { history, database, details }
 
 int selectedRoomId = 0;
+int selectedAdminDatabase = 0;
 
 final _supabase = Supabase.instance.client;
 
@@ -24,38 +25,47 @@ class _HistoryPageState extends State<HistoryPage> {
   Widget build(BuildContext context) {
     return Material(
       color: kPageBackgroundColor,
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: DoubleButton(
-              onFirstOptionChosen: () {
-                setState(() {
-                  selectedView = View.history;
-                });
-              },
-              onSecondOptionChosen: () {
-                setState(() {
-                  selectedView = View.database;
-                });
-              },
-              leftOptionLabel: 'Historia',
-              rightOptionLabel: 'Baza danych',
-            ),
-          ),
-          if (selectedView == View.history)
-            const Expanded(
-              child: WhiteCard(
-                child: ReportsStream(),
+      child: FutureBuilder<String>(
+        future: getUserRole(),
+        builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+          return Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: DoubleButton(
+                  onFirstOptionChosen: () {
+                    setState(() {
+                      selectedView = View.history;
+                    });
+                  },
+                  onSecondOptionChosen: () {
+                    setState(() {
+                      selectedView = View.database;
+                    });
+                  },
+                  leftOptionLabel: 'Historia',
+                  rightOptionLabel: 'Baza danych',
+                ),
               ),
-            )
-          else if (selectedView == View.database)
-            const Expanded(
-              child: WhiteCard(
-                child: RoomsStream(),
-              ),
-            )
-        ],
+              if (selectedView == View.history)
+                const Expanded(
+                  child: WhiteCard(
+                    child: ReportsStream(),
+                  ),
+                )
+              else if (selectedView == View.database)
+                if (snapshot.data == 'admin')
+                  if (selectedAdminDatabase == 0)
+                    const AdminDatabaseMenu()
+                  else
+                    const Expanded(
+                      child: WhiteCard(
+                        child: RoomsStream(),
+                      ),
+                    )
+            ],
+          );
+        },
       ),
     );
   }
@@ -204,5 +214,136 @@ class _ItemsStreamState extends State<ItemsStream> {
             children: itemsListItems,
           );
         });
+  }
+}
+
+class UsersStream extends StatefulWidget {
+  const UsersStream({super.key});
+
+  @override
+  State<UsersStream> createState() => _UsersStreamState();
+}
+
+class _UsersStreamState extends State<UsersStream> {
+  final _stream = _supabase.from('roles').stream(primaryKey: ['id']);
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+        stream: _stream,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(
+              child: CircularProgressIndicator(
+                color: kFuriousRedColor,
+                backgroundColor: kLightGrey,
+              ),
+            );
+          }
+          final users = snapshot.data;
+          List<BasicListItem> itemsListItems = [];
+          for (var user in users!) {
+            final name = user['name'];
+            final status = user['status'];
+            final email = user['email'];
+            final userString = name + ' ' + status + ' ' + email;
+            itemsListItems.add(BasicListItem(
+              onTap: () {},
+              buttonTitle: userString,
+            ));
+          }
+          return ListView(
+            children: itemsListItems,
+          );
+        });
+  }
+}
+
+Future<String> getUserRole() async {
+  var response = await _supabase
+      .from('roles')
+      .select('status')
+      .eq('user_id', _supabase.auth.currentUser?.id)
+      .single();
+
+  return response['status'] as String;
+}
+
+class AdminDatabaseMenu extends StatefulWidget {
+  const AdminDatabaseMenu({super.key});
+
+  @override
+  State<AdminDatabaseMenu> createState() => _AdminDatabaseMenuState();
+}
+
+class _AdminDatabaseMenuState extends State<AdminDatabaseMenu> {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: WhiteCard(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Wybierz bazę',
+                style: TextStyle(fontSize: 20),
+              ),
+              kBigGap,
+              SizedBox(
+                width: kScreenWidth,
+                child: BigWhiteButton(
+                  buttonTitle: 'Lokalizacje',
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const RoomsScreen()));
+                  },
+                ),
+              ),
+              kBigGap,
+              SizedBox(
+                  width: kScreenWidth,
+                  child: BigWhiteButton(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const UsersScreen()));
+                      },
+                      buttonTitle: 'Użytkownicy')),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class RoomsScreen extends StatelessWidget {
+  const RoomsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const WhiteCard(
+        child: RoomsStream(),
+      ),
+    );
+  }
+}
+
+class UsersScreen extends StatelessWidget {
+  const UsersScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const WhiteCard(
+        child: UsersStream(),
+      ),
+    );
   }
 }
